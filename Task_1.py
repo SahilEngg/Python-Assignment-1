03603fe5b2fbe6499dfbe8ee2761fb976dd82ef8
SahilEngg <sahilprajapati2315@gmail.com>
2025-09-10 12:29:54 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+# Task 1: Perform Basic Mathematical Operations
+
+a = int(input("Enter first number: "))
+b = int(input("Enter Second Number: "))
+
+add = a + b
+sub = a - b
+Mul = a * b
+div = a / b
+
+print(f"Addition: {add} \nSubtraction: {sub} \nMultiplication: {Mul} \nDivision: {div}")
