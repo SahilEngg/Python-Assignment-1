14cf9663271e2c277b85482d1f55f8d70cacb611
SahilEngg <sahilprajapati2315@gmail.com>
2025-09-10 12:34:29 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,6 @@
+# Task 2: Create a Personalized Greeting
+
+fname = input("Enter your First name: ")
+lname = input("Enter your Last name: ")
+
+print(f"Hello, {fname} {lname}! Welcome to the python program.")
